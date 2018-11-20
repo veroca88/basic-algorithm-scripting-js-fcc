c7925b7a434bbf4cab3eb5ab8ccbe4c64c9d639f
veronica <veronicadonoso1506@gmail.com>
2018-11-20 09:41:55 -0500

    04-largestNumber js exercice done

--- 04-largestNumber.js
+++ 04-largestNumber.js
@@ -4,7 +4,7 @@ For simplicity, the provided array will contain exactly 4 sub-arrays.
 Remember, you can iterate through an array with a simple for loop,
 and access each member with array syntax arr[i]. */
 function largestOfFour (arr) {
-  var largestNumber = [0, 0, 0, 0]
+  var largestNumber = [-9999, -9999, -9999, -9999]
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++) {
       if (arr[i][j] > largestNumber[i]) {
@@ -12,7 +12,6 @@ function largestOfFour (arr) {
       }
     }
   }
-  console.log(largestNumber)
+  return largestNumber
 }
-
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]])
