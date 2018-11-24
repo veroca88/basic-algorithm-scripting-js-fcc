de7b53e69820882a42c3f633363a2a0a48b3c1b8
veronica <veronicadonoso1506@gmail.com>
2018-11-24 17:19:10 -0500

    07-truncateString js exercice done

---
+++ 07-truncateString.js
@@ -0,0 +1,11 @@
+/* Basic Algorithm Scripting: Truncate a String
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. */
+function truncateString (str, num) {
+  if (str.length > num) {
+    return (str.substring(0, num) + '...')
+  } else if (str.length <= num) {
+    return str
+  }
+}
+
+truncateString('A-tisket a-tasket A green and yellow basket', 8)
