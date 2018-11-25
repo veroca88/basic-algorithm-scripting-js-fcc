7e4deba9d171648ae1bca91808f8589751de31e0
veronica <veronicadonoso1506@gmail.com>
2018-11-24 20:07:08 -0500

    itsBoolean js exercice done

---
+++ itsBoolean.js
@@ -0,0 +1,11 @@
+/* Basic Algorithm Scripting: Boo who
+Check if a value is classified as a boolean primitive. Return true or false. */
+function booWho (bool) {
+  if (typeof (bool) === 'boolean') {
+    return true
+  } else {
+    return false
+  }
+}
+
+booWho(null)
